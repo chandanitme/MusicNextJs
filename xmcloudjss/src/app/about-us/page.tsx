f92ee506215cf95fdc5af7be4731e4ef81849ae0
chandankumarda <chandan.itme@gmail.com>
2025-07-08 13:44:41 +0530

    xmcloud jss

---
+++ xmcloudjss/src/app/about-us/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutUsPage = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our About Us page. Learn more about our mission, vision, and team.</p>
+    </div>
+  );
+};
+
+export default AboutUsPage;
